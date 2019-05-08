2cd4e0bf8544f976a03c1e900a6845ed26cae8d8
ale235 <alejandrocolautti235@hotmail.com>
2019-05-07 23:25:04 -0300

    rt
    rtr

--- resources/views/presupuesto/show.blade.php
+++ resources/views/presupuesto/show.blade.php
@@ -1,35 +1,41 @@
 @extends ('layouts.admin')
 @section ('contenido')
     <div class="col-lg-12">
-        <table class="tg" align="center" width="100%" style="border:1px solid #000">
-            <tr>
-                <th class="tg-baqh" colspan="5">PRESUPESTO - N°: {{$presupuesto->id}}</th>
+        <table class="tg table " align="center" width="100%" style="border:2px solid #000; border-radius: 10px;">
+            <tr style="background-color: #9f191f;">
+                <th class="tg-baqh" colspan="5">
+                    <div style=" display: block">
+                        <span><strong style="color: white">PRESUPESTO - N°: {{$presupuesto->id}}</strong></span>
+                        <span style="color: white; float: right">Fecha: {{now()->format('d/m/y')}}</span>
+                    </div>
+                </th>
             </tr>
+
             <tr>
-                <td class="tg-wp8o" colspan="2"> REPUESTOS TOYOTER</td>
-                <td class="tg-73oq"></td>
-                <td class="tg-73oq" colspan="2">01/02/2019</td>
+                <td class="tg-wp8o" colspan="5"><img src="{{asset('/images/toyoterlogo.png')}}" width="20%"></td>
+                {{--<td class="tg-73oq">Fecha de Hoy: </td>--}}
+                {{--<td class="tg-73oq" colspan="2">01/02/2019</td>--}}
             </tr>
             <tr>
                 <td class="tg-73oq">Razón social:</td>
                 <td class="tg-73oq">Norberto Sodero</td>
-                <td class="tg-73oq"></td>
+                {{--<td class="tg-73oq"></td>--}}
                 <td class="tg-73oq">CUIT:</td>
-                <td class="tg-73oq">20-24876880-1</td>
+                <td class="tg-73oq" colspan="2">20-24876880-1</td>
             </tr>
             <tr>
                 <td class="tg-73oq">Domicilio comercial:</td>
                 <td class="tg-73oq">San Juan 2653 Sta Fe</td>
-                <td class="tg-73oq"></td>
+                {{--<td class="tg-73oq"></td>--}}
                 <td class="tg-73oq">IIBB:</td>
-                <td class="tg-73oq">921-3907013-0</td>
+                <td class="tg-73oq" colspan="2">921-3907013-0</td>
             </tr>
             <tr>
                 <td class="tg-73oq">Cond IVA:</td>
                 <td class="tg-73oq">Inscrito</td>
-                <td class="tg-73oq"></td>
+                {{--<td class="tg-73oq"></td>--}}
                 <td class="tg-73oq">Fecha Inicio de Actividades:</td>
-                <td class="tg-73oq">01/02/2007</td>
+                <td class="tg-73oq" colspan="2">01/02/2007</td>
             </tr>
             <tr>
                 <td class="tg-0lax" colspan="5"></td>
@@ -37,16 +43,16 @@
             <tr>
                 <td class="tg-0lax">CUIT: {{$cliente->cuit}}</td>
                 <td class="tg-0lax"></td>
-                <td class="tg-0lax"></td>
+                {{--<td class="tg-0lax"></td>--}}
                 <td class="tg-0lax">Apellido y Nombre / Razón Social:</td>
-                <td class="tg-0lax"></td>
+                <td class="tg-0lax" colspan="2"></td>
             </tr>
             <tr>
                 <td class="tg-0lax">Cond. IVA:</td>
                 <td class="tg-0lax"></td>
-                <td class="tg-0lax"></td>
+                {{--<td class="tg-0lax"></td>--}}
                 <td class="tg-0lax">Domicilio:</td>
-                <td class="tg-0lax"></td>
+                <td class="tg-0lax" colspan="2"></td>
             </tr>
             <tr>
                 <td class="tg-0lax">Chasis:</td>
